4c0ff4837c459e5244bf242981ba8902872e3a8f
fb_20 <fb_20@ELYASA>
2024-11-24 14:03:02 +0300

    Add project files.

---
+++ Constructor_Console/Ogrenci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Console
+{
+    internal class Ogrenci
+    {
+        //public Ogrenci()
+        //{
+        //    Console.WriteLine("Adı: Elyasa");
+        //    Console.WriteLine("Soyadı: Kapucu");
+        //    Console.WriteLine("Mesleği: Yazılım Mühendisi");
+
+
+        public Ogrenci(string bilgi, string isim,string soyisim,string meslek,int yasi) 
+        {
+            Console.WriteLine("Ad: " + isim);
+            Console.WriteLine("Soy Ad: "+ soyisim);
+            Console.WriteLine("Mesleğiniz: "+ meslek);
+            Console.WriteLine("Yaşınız: " + yasi);
+            Console.WriteLine("Olduğunuz Kurum: " + bilgi);
+        }
+    }
+}
---
+++ Constructor_Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ogrenci ogr = new Ogrenci();
+            //Console.Read();
+
+            string blg,isim,soyisim,meslek;
+            int yasi;
+            Console.Write("Adı: ");
+            isim = Console.ReadLine();
+            Console.Write("Soyadı: ");
+            soyisim = Console.ReadLine();
+            Console.Write("Mesleğiniz: ");
+            meslek = Console.ReadLine();
+            Console.Write("Yaşınız: ");
+            yasi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Olduğunuz Kurum: ");
+            blg = Console.ReadLine();
+            Ogrenci ogr = new Ogrenci(blg,isim,soyisim,meslek,yasi);
+            Console.Read();
+        }
+    }
+}
